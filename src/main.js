c13971e281cdf5499fa5de38df0465a1da7e26e4
Slick Jurgen <slickjurgen@googlemail.com>
2021-05-16 16:43:19 +0200

    Added apollo provider and config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueApollo from "vue-apollo";
+
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+Vue.use(VueApollo);
+
 Vue.config.productionTip = false
 
+const getHeaders = () => {
+  const headers = {};
+   const token = window.localStorage.getItem('apollo-token');
+   if (token) {
+     headers.authorization = `Bearer ${token}`;
+   }
+   return headers;
+ };
+
+ // Create an http link:
+const link = new HttpLink({
+  //uri: 'https://hasura.io/learn/graphql',
+  uri: 'http://localhost:8080/v1/graphql',
+  fetch,
+  headers: getHeaders()
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+})
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  apolloProvider,
+  render: h => h(App)
+}).$mount("#app");
